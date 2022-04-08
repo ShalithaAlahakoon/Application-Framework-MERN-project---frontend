38092226d8fdb1c2eaa2613cc9bdd88fa8b4aa3e
shalitha alahakoon <79890761+ShalithaAlahakoon@users.noreply.github.com>
2022-04-08 15:20:54 +0530

    Header inserted

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './assets/css/index.css';
 import reportWebVitals from './reportWebVitals';
 import Header from './components/header';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header/>
-    <App />
+    <Header />
+
+
   </React.StrictMode>,
   document.getElementById('root')
 );
