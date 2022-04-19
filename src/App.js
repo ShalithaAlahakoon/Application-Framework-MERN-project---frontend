54aa741b96fe679a6408806df63619a839b15c6f
ShalithAlahakoon <shalitha97068@gmail.com>
2022-04-19 09:44:57 +0530

    backup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
-import Header from "./components/Header";
-import AdminSideNav from "./components/AdminSideNav";
+// import Header from "./components/Header";
+// import AdminSideNav from "./components/AdminSideNav";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./assets/css/App.css";
-import Home from "./pages/Home";
+import AdminHome from "./pages/Admin/AdminHome";
 import Reports from "./pages/Admin/Report";
 import Students from "./pages/Admin/Students";
+import Login from "./pages/Authenticaiton/Login";
+import Register from "./pages/Authenticaiton/Register";
+import StudentHome from "./pages/Student/StudentHome";  
+
 
 
 function App() {
     return (
         <>
             <Router>
-                <Header />
-                <AdminSideNav />
                 <Routes>
-                    <Route path="/" exact element={<Home/>} />
+                    <Route path="/" exact element={<Login/>} />
+                    <Route path="/admin" exact element={<AdminHome/>} />
+                    <Route path="/student" exact element={<StudentHome/>} />
+                    <Route path="/register" exact element={<Register/>} />
                     <Route path="/students" element={<Students/>} />
                     <Route path="/reports" element={<Reports/>} />
                 </Routes>
---
+++ src/pages/Authenticaiton/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/Header'
+
+function Register() {
+  return (
+    <div>
+        <Header />
+      <h1>register page</h1>
+    </div>
+  )
+}
+
+export default Register
