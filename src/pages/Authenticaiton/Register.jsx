428a6caf5b333f47af6800f4ed0dae1e2d72e9e4
SandunDharmadasa <sandundharmaasa0621@gmail.com>
2022-05-28 14:04:02 +0530

    Created the Frontend of the Register Page

--- src/pages/Authenticaiton/Register.jsx
+++ src/pages/Authenticaiton/Register.jsx
@@ -1,12 +1,111 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from '../../components/Header'
+import axios from 'axios';
 
 function Register() {
+  const [id,setID] = useState("");
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [mobileNumber,setMobileNumber] = useState("");
+  const [password,setPassword] = useState("");
+  const [password2,setPassword2] = useState("");
+
+  function sendData(e){
+    e.preventDefault();
+
+    const newUser = {
+      id:id,
+      name:name,
+      email:email,
+      mobileNumber:mobileNumber,
+      password:password,
+      password2:password2
+    }
+
+    axios.post('#',newUser).then(()=>{
+      console.log("Registered Successfully")
+      setID("");
+      setName("");
+      setMobileNumber("");
+      setPassword("");
+      setPassword2("");
+
+    }).catch((err)=>{
+      alert(err)
+    })
+  }
+
   return (
-    <div>
-        <Header />
-      <h1>register page</h1>
+    <>
+    <Header />
+    <div className='container'>
+        <div>
+          <h1>Register</h1>
+      </div>
+      <form onSubmit={sendData}>
+      <div className="form-outline mb-4">
+          <label for='id'>SlIIT ID Number</label><br/>
+          <input type="text" className="form-control" id="name"  placeholder="Student ID / Staff ID" 
+          onChange={(e)=>{
+            setID(e.target.value);
+          }} />
+          
+      </div>
+      
+      <div className="form-outline mb-4">
+          <label for='name'>Full Name</label><br/>
+          <input type="text" className="form-control" id="name"  placeholder="Enter Your Name as in ID" 
+          onChange={(e)=>{
+            setName(e.target.value);
+          }} />
+          
+      </div>
+
+      <div className="form-outline mb-4">
+          <label for='email'>Email Address</label><br/>
+          <input type="email" className="form-control" id="email"  placeholder="example.my.sliit.lk" 
+          onChange={(e)=>{
+            setEmail(e.target.value);
+          }} />
+          
+      </div>
+
+      <div className="form-outline mb-4">
+          <label for='mobileNumber'>Mobile Number</label><br/>
+          <input type="text" className="form-control" id="mobileNumber"   placeholder="Enter Your Mobile Number" 
+          onChange={(e)=>{
+            setMobileNumber(e.target.value);
+          }} />
+          
+      </div>
+      
+      <div className="form-outline mb-4">
+          <label for='password'>Password</label><br/>
+          <input type="password" className="form-control" id="password"  placeholder="Enter a Password" 
+          onChange={(e)=>{
+            setPassword(e.target.value);
+          }} />
+      </div>
+            
+      <div className="form-outline mb-4">
+          <label for='password2'>Confirm the Password</label><br/>
+          <input type="password" className="form-control" id="password2"  placeholder="Retype the Password" 
+          onChange={(e)=>{
+            setPassword2(e.target.value);
+          }} />
+          
+      </div>
+
+      <div>
+      <button type="submit" class="btn btn-primary">Sign Up</button>
+      </div>
+
+      <br/>
+      <p>Already have an account? <a href="/">Login</a></p>
+
+      </form>
     </div>
+    </>
   )
 }
 
