318ba133aa2a950d7b940a0f8e5183a0479f604f
SandunDharmadasa <sandundharmaasa0621@gmail.com>
2022-06-04 23:27:21 +0530

    Frontends of the panel member's tasks

---
+++ src/pages/FilesList.jsx
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from 'react';
+import Header from '../components/Header'
+import SideNav from '../components/SideNav'
+import download from 'downloadjs';
+import axios from 'axios';
+
+const FilesList = () => {
+  const [filesList, setFilesList] = useState([]);
+  const [errorMsg, setErrorMsg] = useState('');
+
+  useEffect(() => {
+    const getFilesList = async () => {
+      try {
+        const { data } = await axios.get('http://localhost:5000/api/document/getAllFiles');
+        setErrorMsg('');
+        setFilesList(data);
+      } catch (error) {
+        error.response && setErrorMsg(error.response.data);
+      }
+    };
+
+    getFilesList();
+  }, []);
+
+  const downloadFile = async (id, path, mimetype) => {
+    try {
+      const result = await axios.get(`http://localhost:5000/api/document/download/${id}`, {
+        responseType: 'blob'
+      });
+      const split = path.split('/');
+      const filename = split[split.length - 1];
+      setErrorMsg('');
+      return download(result.data, filename, mimetype);
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        setErrorMsg('Error while downloading file. Try again later');
+      }
+    }
+  };
+
+  return (
+    <>
+    <Header/>
+    <SideNav/>
+    <div className="container">
+      {errorMsg && <p className="errorMsg">{errorMsg}</p>}
+      <h1>Documents of the Students</h1>
+      <table class="table">
+      <thead>
+                    <tr>
+                        <th scope="col">Document ID</th>
+                        <th scope="col">Document Title</th>
+                        <th scope="col">Group ID</th>
+                        <th scope="col">Download</th>
+                        <th scope="col">Action</th>
+
+                    </tr>
+                </thead>
+        <tbody>
+        {filesList.length > 0 ? (
+            filesList.map((file,index) => (
+              file.type == "Document" ?(
+                <tr key={index}>
+                  <td className="file-id">{file._id}</td>  
+                  <td className="file-title">{file.title}</td>
+                  <td className="file-description">{file.groupID}</td>
+
+
+                  <td>
+                    <a
+                      href="#/"
+                      onClick={() =>
+                        downloadFile(file._id, file.file_path, file.file_mimetype)
+                      }
+                    >
+                      Download
+                    </a>
+                  </td>
+                  <td>
+                  <a className="btn btn-warning" href='/GiveFeedback'>
+                            <i className="far fa-mark"></i>Give a Feedback
+                        </a>
+                    </td>
+                </tr>
+                ):(<tr></tr>)
+              )
+            )
+          ) : (
+            <tr>
+              <td colSpan={3} style={{ fontWeight: '300' }}>
+                No files found. Please add some.
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+      <br/>
+
+      <h1>Presentations of the Students</h1>      
+      <table class="table">
+      <thead>
+                    <tr>
+                        <th scope="col">Presentation ID</th>
+                        <th scope="col">Group ID</th>
+                        <th scope="col">Download</th>
+                        <th scope="col">Action</th>
+
+                    </tr>
+                </thead>
+        <tbody>
+          {filesList.length > 0 ? (
+            filesList.map((file,index) => (
+              file.type == "Presentation" ?(
+                <tr key={index}>
+                  <td className="file-id">{file._id}</td>  
+                  <td className="file-description">{file.groupID}</td>
+
+                  <td>
+                    <a
+                      href="#/"
+                      onClick={() =>
+                        downloadFile(file._id, file.file_path, file.file_mimetype)
+                      }
+                    >
+                      Download
+                    </a>
+                  </td>
+                  <td>
+                        <a className="btn btn-warning" href='/GiveMarks'>
+                            <i className="far fa-mark"></i>Evaluate
+                        </a>
+                    </td>
+                </tr>
+                ):(<tr></tr>)
+              )
+            )
+          ) : (
+            <tr>
+              <td colSpan={3} style={{ fontWeight: '300' }}>
+                No files found. Please add some.
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+    </>
+  );
+};
+
+export default FilesList;
